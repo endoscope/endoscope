7ad060410f24bcc1c7536a1fa164bccee832688a
Pawel Halicz <p.halicz@smartrecruiters.com>
2016-05-15 10:33:05 +0200

    Fixed example data after changing UI path.

--- example-war/src/main/java/com/github/endoscope/example/PopulateUiDataFilter.java
+++ example-war/src/main/java/com/github/endoscope/example/PopulateUiDataFilter.java
@@ -10,8 +10,7 @@ import java.io.IOException;
  * It's demo app so run some processing before entering UI - otherwise it's empty and doesn't look good.
  */
 @WebFilter(filterName = "PopulateUiDataFilter",urlPatterns = {
-        "/rest/endoscope/current/ui/*",
-        "/rest/endoscope/storage/ui/*"
+        "/rest/endoscope/*"
 })
 public class PopulateUiDataFilter implements Filter {
     @Inject
@@ -20,7 +19,7 @@ public class PopulateUiDataFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         String url = ((HttpServletRequest)servletRequest).getRequestURL().toString();
-        if( url.endsWith("/ui") ){//main page only
+        if( url.endsWith("endoscope/") ){//main page only
             controller.process(null);
         }
         filterChain.doFilter(servletRequest, servletResponse);
