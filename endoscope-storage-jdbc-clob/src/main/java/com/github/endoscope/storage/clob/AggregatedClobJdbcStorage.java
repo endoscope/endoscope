6b478d06468e3d90d4ed5dfaa1b8519d620ef575
Pawel Halicz <p.halicz@smartrecruiters.com>
2017-09-08 10:43:33 +0200

    Fixed table prefix in aggregated CLOB storage

--- endoscope-storage-jdbc-clob/src/main/java/com/github/endoscope/storage/clob/AggregatedClobJdbcStorage.java
+++ endoscope-storage-jdbc-clob/src/main/java/com/github/endoscope/storage/clob/AggregatedClobJdbcStorage.java
@@ -7,12 +7,12 @@ import static java.util.Arrays.asList;
 public class AggregatedClobJdbcStorage extends AggregatedStorage {
     @Override
     public void setup(String initParam) {
-        defaultStorage  = new ClobJdbcStorage().setTablePrefix("clob_");
+        defaultStorage  = new ClobJdbcStorage().setTablePrefix("");
         defaultStorage.setup(initParam);
 
-        dailyStorage    = new ClobJdbcStorage().setTablePrefix("clob_day_");
-        weeklyStorage   = new ClobJdbcStorage().setTablePrefix("clob_week_");
-        monthlyStorage  = new ClobJdbcStorage().setTablePrefix("clob_month_");
+        dailyStorage    = new ClobJdbcStorage().setTablePrefix("day_");
+        weeklyStorage   = new ClobJdbcStorage().setTablePrefix("week_");
+        monthlyStorage  = new ClobJdbcStorage().setTablePrefix("month_");
 
         //save DB connection resources and re-use the same DS for aggregated storage
         asList( dailyStorage, weeklyStorage, monthlyStorage)
